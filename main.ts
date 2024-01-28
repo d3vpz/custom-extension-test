eb422bdddbf35f3b4073e6919c6129116e3b2918
Devpz <114014131+d3vpz@users.noreply.github.com>
2024-01-28 18:46:03 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,39 @@
+namespace projection {
+    let focal_len: number;
 
+    //% block
+    export function setFocalLength(focal_length: number) {
+        focal_len = focal_length;
+    }
+
+    //% block
+    export function screenCoordinateX(x: number, z: number): number {
+        let screen_x = ((x+80) / z) * focal_len;
+        return screen_x;
+    }
+
+    //% block
+    export function screenCoordinateY(y: number, z: number): number {
+        let screen_y = ((y+60) / z) * focal_len;
+        return screen_y;
+    }
+    
+    //% block="draw $c 3D line on $surface at $x1 $y1 $z1 $x2 $y2 $z2"
+    //% surface.shadow="speedPicker"
+    export function line_3d(surface: Image, x1: number, y1: number, z1: number, x2: number, y2: number, z2: number, c: number) {
+        let sx1 = screenCoordinateX(x1, z1);
+        let sy1 = screenCoordinateY(y1, z1);
+        let sx2 = screenCoordinateX(x2, z2);
+        let sy2 = screenCoordinateY(y2, z2);
+        surface.drawLine(sx1, sy1, sx2, sy2, c);
+    }
+
+    //% block="draw $c plane on $surface at $x $y $z with $width $height"
+    //% surface.shadow="speedPicker"
+    export function plane(surface: Image, x: number, y: number, z: number, width: number, height: number, c: number) {
+        line_3d(surface, x, y, z, x + width, y, z, c);
+        line_3d(surface, x + width, y, z, x + width, y + height, z, c);
+        line_3d(surface, x + width, y + height, z, x, y + height, z, c);
+        line_3d(surface, x, y + height, z, x, y, z, c);
+    }
+}
