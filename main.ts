72697e123756e1e1cb66d273c3c617ecda9918b6
Devpz <114014131+d3vpz@users.noreply.github.com>
2024-01-29 14:01:27 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
-//% color="#538a74"
+//% color="000000"
 namespace projection {
     let focal_len: number;
-    let vertices: number = [];
+    let vertices: Array<any> = [];
 
     let camera: Object = {
         x: 0,
@@ -18,20 +18,21 @@ namespace projection {
     export function addVertex(x: number, y: number, z: number) {
         vertices.push([x, y, z]);
     }
-    
-    //% block="draw $c 3D line on $surface at $x1 $y1 $z1 $x2 $y2 $z2"
+
+    //% block="draw $c 3D line at $x1 $y1 $z1 $x2 $y2 $z2"
     //% surface.shadow="speedPicker"
-    export function line_3d(surface: Image, x1: number, y1: number, z1: number, x2: number, y2: number, z2: number, c: number) {
-        surface.drawLine(sx1, sy1, sx2, sy2, c);
+    export function line_3d(x1: number, y1: number, z1: number, x2: number, y2: number, z2: number, c: number) {
+        addVertex(x1, y1, z1);
+        addVertex(x2, y2, z2);
     }
 
     //% block="draw $c plane on $surface at $x $y $z with $width $height"
     //% surface.shadow="speedPicker"
     export function plane(surface: Image, x: number, y: number, z: number, width: number, height: number, c: number) {
-        line_3d(surface, x, y, z, x + width, y, z, c);
-        line_3d(surface, x + width, y, z, x + width, y + height, z, c);
-        line_3d(surface, x + width, y + height, z, x, y + height, z, c);
-        line_3d(surface, x, y + height, z, x, y, z, c);
+        line_3d(x, y, z, x + width, y, z, c);
+        line_3d(x + width, y, z, x + width, y + height, z, c);
+        line_3d(x + width, y + height, z, x, y + height, z, c);
+        line_3d(x, y + height, z, x, y, z, c);
     }
 
     //% block="PI"
@@ -41,16 +42,16 @@ namespace projection {
 
     //% block="degrees to radians: $deg"
     export function deg2rad(deg: number): number {
-        return deg * (pi()/180);
+        return deg * (pi() / 180);
     }
 
     //% block="radians to degrees: $rad"
     export function rad2deg(rad: number): number {
-        return rad * (180/pi());
+        return rad * (180 / pi());
     }
 
     //% block="perspective projection $x $y $z"
-    export function perspective(x: number, y: number, z: number): Array {
+    export function perspective(x: number, y: number, z: number): Array<any> {
         let screen_x = x / z * focal_len;
         let screen_y = y / z * focal_len;
         return [screen_x, screen_y];
@@ -58,7 +59,7 @@ namespace projection {
 
     //% block="project vertices on screen"
     export function project_vertices() {
-        for (i=0; i<vertices.length; i++) {
+        for (let i = 0; i < vertices.length; i++) {
         }
     }
 }
