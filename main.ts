c370049db01088f481c7737523b1ec9f7aaac5f1
Devpz <114014131+d3vpz@users.noreply.github.com>
2024-01-30 11:55:34 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -34,14 +34,21 @@ namespace projection {
         surface.drawLine(x1, y1, x2, y2, c);
     }
 
-    //% block="draw $c plane on $surface at $x $y $z with $width $height"
+    //% block="draw $c 3D line on $surface from $x1 $y1 $z1 to $x2 $y2 $z2"
     //% surface.shadow="speedPicker"
-    // export function plane(surface: Image, x: number, y: number, z: number, width: number, height: number, c: number) {
-    //     line_3d(surface, x, y, z, x + width, y, z, c);
-    //     line_3d(surface, x + width, y, z, x + width, y + height, z, c);
-    //     line_3d(surface, x + width, y + height, z, x, y + height, z, c);
-    //     line_3d(surface, x, y + height, z, x, y, z, c);
-    // }
+    export function alt_line_3d(surface: Image, x1: number, y1: number, z2: number, x2: number, y2: number, z2: number, c: number) {
+        let sx1 = x1 / z1 * focal_len;
+        let sy1 = y1 / z1 * focal_len;
+        let sx2 = x2 / z2 * focal_len;
+        let sy2 = y2 / z2 * focal_len;
+        surface.drawLine(sx1, sy1, sx2, sy2, c);
+    }
+
+    //% block="draw $c plane on $surface from $p1 to $p2"
+    //% surface.shadow="speedPicker"
+    export function plane(surface: Image, p1: number, p2: number, c: number) {
+        line_3d(surface,)
+    }
 
     //% block="PI"
     export function pi(): number {
@@ -74,7 +81,6 @@ namespace projection {
             let vertex = vertices[v];
             let screen_coords = perspective(vertex.x, vertex.y, vertex.z);
             screen_points.push(screen_coords);
-            console.log(screen_points);
         }
     }
 }
